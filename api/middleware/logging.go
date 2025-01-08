e31df395b708ba85c866092e0cd81e18049be925
Daniel Afonso <daniel.afonso@parceiros.nos.pt>
2025-01-08 13:59:04 +0000

    Add status code wrapping and logging

--- api/middleware/logging.go
+++ api/middleware/logging.go
@@ -6,10 +6,29 @@ import (
 	"time"
 )
 
+type wrappedWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// overrides the writeheader function, so that teh status code is stored
+func (w *wrappedWriter) WriteHeader(statusCode int) {
+	// calling upper function
+	w.ResponseWriter.WriteHeader(statusCode)
+	// storing status code
+	w.statusCode = statusCode
+}
+
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		next.ServeHTTP(w, r)
-		log.Println(r.Method, r.URL.Path, time.Since(start))
+
+		wrapped := &wrappedWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(wrapped, r)
+		log.Println(wrapped.statusCode, r.Method, r.URL.Path, time.Since(start))
 	})
 }
